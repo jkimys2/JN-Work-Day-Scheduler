1a7191e9a1b1bf17ea616557595e463576e928a9
Jordan <jkimys2@gmail.com>
2023-12-14 18:35:34 -0500

    js update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,19 @@
 // in the html.
 var submitBtn = $(".saveBtn");
 var currentTime = dayjs().format("HH");
-// var hourSelected = {
-//   hour9: $("#hour-9").children(".hour").text(),
-//   hour10: $("#hour-10"),
-//   hour11: $("#hour-11"),
-//   hour12: $("#hour-12"),
-//   hour13: $("#hour-1"),
-//   hour14: $("#hour-2"),
-//   hour15: $("#hour-3"),
-//   hour16: $("#hour-4"),
-//   hour17: $("#hour-5"),
-// };
+;
+
+var hourSelected = {
+  hour9: $("#hour-9").children(".hour").text(),
+  hour10: $("#hour-10"),
+  hour11: $("#hour-11"),
+  hour12: $("#hour-12"),
+  hour13: $("#hour-1"),
+  hour14: $("#hour-2"),
+  hour15: $("#hour-3"),
+  hour16: $("#hour-4"),
+  hour17: $("#hour-5"),
+};
 // console.log(hourSelected)
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -27,27 +29,34 @@ $(function () {
     event.preventDefault();
     var hour = $(this).siblings(".hour").text();
     var toDo = $(this).siblings(".description").val();
-    var userInput = {
-      time: hour,
-      todo: toDo,
-    };
-    if (userInput) {
-      var newInput = {
-        time: hour,
-        todo: toDo,
-      };
-      userInput = JSON.parse(localStorage.getItem("To-Do's")) || [];
-      userInput.push(newInput);
-      localStorage.setItem("To-Do's", JSON.stringify(userInput));
-    }
+    // if (userInput) {
+    //   var newInput = {
+    //     time: hour,
+    //     todo: toDo,
+    //   };
+    //   userInput = JSON.parse(localStorage.getItem("To-Do's")) || [];
+    //   userInput.push($(newInput));
+    //   localStorage.setItem("To-Do's", JSON.stringify(userInput));
+    // }
+    // userInput = JSON.parse(localStorage.getItem("To-Do's")) || [];
 
-    // renderMessage();
+    localStorage.setItem(hour, toDo);
+    renderMessage();
   });
-  // function renderMessage() {
-  //   var saveEl = $("<p>");
-  //   saveEl.text("Appointment added to LocalStorage ✅");
-  //   headerEl.append(saveEl);
-  // }
+
+  function displayToDo() {
+    $.each(hourSelected, function () {
+      toDo.text(localStorage.getItem(hour, toDo));
+    });
+  }
+  displayToDo();
+
+  function renderMessage() {
+    var headerEl = $("lead")
+    var saveEl = $("<p>");
+    saveEl.text("Appointment added to LocalStorage ✅");
+    headerEl.append(saveEl);
+  }
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
