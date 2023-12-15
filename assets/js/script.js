74bcd2c31ee81bb513af86c781db4945bcf4f00e
Jordan <jkimys2@gmail.com>
2023-12-15 14:30:26 -0500

    js local storage update:

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,18 @@
 // in the html.
 var submitBtn = $(".saveBtn");
 var currentTime = dayjs().format("HH");
-;
+var hourEl = $(".hour");
+var textEl = $(".description");
+var hour9 = $("#hour-9");
+var hour10 = $("#hour-10");
+var hour11 = $("#hour-11");
+var hour12 = $("#hour-12");
+var hour13 = $("#hour-1");
+var hour14 = $("#hour-2");
+var hour15 = $("#hour-3");
+var hour16 = $("#hour-4");
+var hour17 = $("#hour-5");
 
-var hourSelected = {
-  hour9: $("#hour-9").children(".hour").text(),
-  hour10: $("#hour-10"),
-  hour11: $("#hour-11"),
-  hour12: $("#hour-12"),
-  hour13: $("#hour-1"),
-  hour14: $("#hour-2"),
-  hour15: $("#hour-3"),
-  hour16: $("#hour-4"),
-  hour17: $("#hour-5"),
-};
 // console.log(hourSelected)
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -24,35 +23,30 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  function displayToDo() {
+    hour9.children(1).val(localStorage.getItem("9AM"));
+    hour10.children(1).val(localStorage.getItem("10AM"));
+    hour11.children(1).val(localStorage.getItem("11AM"));
+    hour12.children(1).val(localStorage.getItem("12PM"));
+    hour13.children(1).val(localStorage.getItem("1PM"));
+    hour14.children(1).val(localStorage.getItem("2PM"));
+    hour15.children(1).val(localStorage.getItem("3PM"));
+    hour16.children(1).val(localStorage.getItem("4PM"));
+    hour17.children(1).val(localStorage.getItem("5PM"));
+  }
+  displayToDo();
 
   submitBtn.on("click", function (event) {
     event.preventDefault();
     var hour = $(this).siblings(".hour").text();
     var toDo = $(this).siblings(".description").val();
-    // if (userInput) {
-    //   var newInput = {
-    //     time: hour,
-    //     todo: toDo,
-    //   };
-    //   userInput = JSON.parse(localStorage.getItem("To-Do's")) || [];
-    //   userInput.push($(newInput));
-    //   localStorage.setItem("To-Do's", JSON.stringify(userInput));
-    // }
-    // userInput = JSON.parse(localStorage.getItem("To-Do's")) || [];
 
     localStorage.setItem(hour, toDo);
     renderMessage();
   });
 
-  function displayToDo() {
-    $.each(hourSelected, function () {
-      toDo.text(localStorage.getItem(hour, toDo));
-    });
-  }
-  displayToDo();
-
   function renderMessage() {
-    var headerEl = $("lead")
+    var headerEl = $("lead");
     var saveEl = $("<p>");
     saveEl.text("Appointment added to LocalStorage ✅");
     headerEl.append(saveEl);
