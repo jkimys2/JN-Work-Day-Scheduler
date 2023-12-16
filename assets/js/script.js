7ee55f02e76736e9db9209a0406d10ec4ea994b3
Jordan <jkimys2@gmail.com>
2023-12-16 10:52:29 -0500

    js update

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,8 @@ var submitBtn = $(".saveBtn");
 var currentHour = dayjs().hour();
 var hourEl = $(".hour");
 var textEl = $(".description");
-var hourBlockEl = {
-  hour9: $("#hour-9 .time-block"),
-  hour10: $("#hour-10 .time-block"),
-  hour11: $("#hour-11 .time-block"),
-  hour12: $("#hour-12 .time-block"),
-  hour13: $("#hour-1 .time-block"),
-  hour14: $("#hour-2 .time-block"),
-  hour15: $("#hour-3 .time-block"),
-  hour16: $("#hour-4 .time-block"),
-  hour17: $("#hour-5 .time-block"),
-};
+var headerEl = $("#heading");
+var saveEl = $("<div>");
 
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
@@ -30,16 +21,15 @@ $(function () {
     event.preventDefault();
     var hour = $(this).parent().attr("id");
     var toDo = $(this).siblings(".description").val();
-console.log(hour)
     localStorage.setItem(hour, toDo);
     renderMessage();
   });
 
   function renderMessage() {
-    var headerEl = $("#heading");
-    var saveEl = $("<p>");
-    saveEl.text("Appointment added to localStorage ✅");
-    headerEl.append(saveEl);
+    saveEl.empty()
+    var pTag = $("<p>")
+    pTag.text("Appointment added to localStorage ✅");
+    headerEl.append(saveEl.append(pTag));
   }
 
   // TODO: Add code to apply the past, present, or future class to each time
@@ -51,8 +41,7 @@ console.log(hour)
  
   function timeBlockColor() {
     $(".time-block").each(function () {
-      // var hourNumber = hourBlockEl.replace(/hour/, "");
-      // console.log(hourNumber)
+
 var rowHour = parseInt($(this).attr("id").split("-").pop());
 if (rowHour < currentHour) {
   $(this).children(".description").removeClass("present")
@@ -80,15 +69,6 @@ if (rowHour < currentHour) {
     for (var i = 9; i < 18; i++) {
       $("#hour-"+i).children(1).val(localStorage.getItem("hour-"+i))
     }
-    // $("#hour-9").children(1).val(localStorage.getItem("9AM"));
-    // $("#hour-10").children(1).val(localStorage.getItem("10AM"));
-    // $("#hour-11").children(1).val(localStorage.getItem("11AM"));
-    // $("#hour-12").children(1).val(localStorage.getItem("12PM"));
-    // $("#hour-1").children(1).val(localStorage.getItem("1PM"));
-    // $("#hour-2").children(1).val(localStorage.getItem("2PM"));
-    // $("#hour-3").children(1).val(localStorage.getItem("3PM"));
-    // $("#hour-4").children(1).val(localStorage.getItem("4PM"));
-    // $("#hour-5").children(1).val(localStorage.getItem("5PM"));
   }
 
   // TODO: Add code to display the current date in the header of the page.
